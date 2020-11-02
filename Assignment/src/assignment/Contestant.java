d392973afef81085e4eb6bf0202583ec117cbfcb
Phuc Lee <lephuc2605@gmail.com>
2020-11-02 09:01:47 +0700

    Create contestant class

--- Assignment/src/assignment/Contestant.java
+++ Assignment/src/assignment/Contestant.java
@@ -1,5 +1,67 @@
 package assignment;
 
 public class Contestant {
+    private String name;
+    private String id;
+    private String email;
+    private String mobilephone;
+    private int rank;
+
+    public Contestant() {
+    }
+
+    public Contestant(String name, String id, String email, String mobilephone, int rank) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.mobilephone = mobilephone;
+        this.rank = rank;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobilephone() {
+        return mobilephone;
+    }
+
+    public void setMobilephone(String mobilephone) {
+        this.mobilephone = mobilephone;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Contestant{" + "name=" + name + ", id=" + id + ", email=" + email + ", mobilephone=" + mobilephone + ", rank=" + rank + '}';
+    }
+    
     
 }
