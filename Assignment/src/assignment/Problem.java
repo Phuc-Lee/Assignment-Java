7bfb137bc0c9239904d6080dbd512ba55e50c2d8
Trần Tiến Thịnh <phihot8888@gmail.com>
2020-11-02 09:01:11 +0700

    Create Problem class

---
+++ Assignment/src/assignment/Problem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Problem {
+    private String problem;
+    private String date;
+    private String name;
+    private String short_decrip;
+    private String long_decrip;
+    private double mark_weight;
+    private int category;
+    private String author;
+    
+}
