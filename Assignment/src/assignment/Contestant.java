1afcc18a36e7e31ec7936fb79a7843c0b758e7f1
Trần Tiến Thịnh <phihot8888@gmail.com>
2020-11-04 09:31:08 +0700

    update hàm change infor contestant
    update phiên bản sơ khai :V

--- Assignment/src/assignment/Contestant.java
+++ Assignment/src/assignment/Contestant.java
@@ -1,11 +1,15 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Contestant {
+
     private String name;
     private String id;
     private String email;
     private String mobilephone;
     private int rank;
+    private String password;
 
     public Contestant() {
     }
@@ -17,7 +21,7 @@ public class Contestant {
         this.mobilephone = mobilephone;
         this.rank = rank;
     }
-    
+
     public String getName() {
         return name;
     }
@@ -58,10 +62,24 @@ public class Contestant {
         this.rank = rank;
     }
 
-    @Override
-    public String toString() {
-        return "Contestant{" + "name=" + name + ", id=" + id + ", email=" + email + ", mobilephone=" + mobilephone + ", rank=" + rank + '}';
+//    @Override
+//    public String toString() {
+//        return "Contestant{" + "name=" + name + ", id=" + id + ", email=" + email + ", mobilephone=" + mobilephone + ", rank=" + rank + '}';
+//    }
+    public void changeInfor() {
+        System.out.println("Change contestant's information ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name : ");
+        String newName = sc.nextLine();
+        System.out.println("Email : ");
+        String newEmail = sc.nextLine();
+        System.out.println("MobilePhone : ");
+        int newPhone = sc.nextInt();
+        while(newPhone <= 1000000000 || (newPhone / 1000000000) > 100){
+            System.out.println("Invalid Phone Number , please re-enter :");
+            newPhone = sc.nextInt();
+        }
+        System.out.println("Password : ");
+        String newPass = sc.nextLine();
     }
-    
-    
 }
