5e708c35d780193483f01dac3e9b181601d8dd9b
Phuc Lee <lephuc2605@gmail.com>
2020-11-07 00:17:59 +0700

    rename login_info.txt and create questionBank.dat and tested writing and reading .dat file

--- Assignment/src/assignment/Assignment.java
+++ Assignment/src/assignment/Assignment.java
@@ -1,8 +1,24 @@
 package assignment;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Assignment {
     public static void main(String[] args) {
-
+        try {
+            FileWriter writeToFile  = new FileWriter("questionBank.dat",true);  // true is for appending to the end of the file
+            writeToFile.append("This is a test for reading and writing file!\n");
+            writeToFile.close();
+            String[] lines = Files.readAllLines(new File("questionBank.dat").toPath()).toArray(new String[0]);
+            System.out.println(lines.length);
+            for(String i: lines){
+                System.out.println(i);
+            }
+        } catch (IOException ex) {
+            System.out.println("You have not put question bank file!");
+        }
     }
     
 }
