aded5e97b801284854bcd3b3d6fb5cb6654d84f4
Phuc Lee <lephuc2605@gmail.com>
2020-11-09 10:19:34 +0700

    completed login method

--- Assignment/src/assignment/Assignment.java
+++ Assignment/src/assignment/Assignment.java
@@ -4,42 +4,39 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Assignment {
     public static void main(String[] args) {
         boolean logIn = logIn();
         
-        if(logIn)
-        
-        try {
-            FileWriter writeToFile  = new FileWriter("questionBank.dat",true);  // true is for appending to the end of the file
-            writeToFile.append("This is a test for reading and writing file!\n");
-            writeToFile.close();
-            String[] lines = Files.readAllLines(new File("questionBank.dat").toPath()).toArray(new String[0]);
-            
-        } catch (IOException ex) {
-            System.out.println("You have not put question bank file!");
+        if(logIn){
+            System.out.println("hello");
         }
+        
+//        try {
+//            FileWriter writeToFile  = new FileWriter("questionBank.dat",true);  // true is for appending to the end of the file
+//            writeToFile.append("This is a test for reading and writing file!\n");
+//            writeToFile.close();
+//            String[] lines = Files.readAllLines(new File("questionBank.dat").toPath()).toArray(new String[0]);
+//            
+//        } catch (IOException ex) {
+//            System.out.println("You have not put question bank file!");
+//        }
     }
     
     static boolean logIn(){
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("User name: ");
-        String name = sc.next();
-        boolean checkName = checkName(name);
+        String name = sc.nextLine();
+        boolean checkName = check(name,'n');
         System.out.print("Password: ");
         String pass = sc.nextLine();
-        boolean checkPass = checkPass(pass);
-        if(!checkName){
-            System.out.println("Your user name does not exist. Would you like to sign in? (y/n)");
-            String choice = sc.next();
-            if(choice.equals("y")){
-                signIn();
-            }else{
-                logIn();
-            }
-        }
+        boolean checkPass = check(pass,'p');
+
         if(checkName && checkPass){
             System.out.println("Welcome back!");
             return true;
@@ -53,11 +50,38 @@ public class Assignment {
         
     }
     
-    static boolean checkName(String name){
-        return true;
-    }
-    
-    static boolean checkPass(String pass){
-        return true;
+    static boolean check(String str, char a){
+        HashMap<String, String> loginfo = new HashMap<>();
+        try {
+            Scanner sc1 = new Scanner(new File("contestant.dat"));
+            while (sc1.hasNext()){
+                String[] split = sc1.next().split("~");
+                loginfo.put(split[0], split[1]);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        boolean i = true;
+        
+        
+        for (Map.Entry ele : loginfo.entrySet()) {
+//            System.out.println(ele);
+            if (a == 'n') {
+                if (str.equals(ele.getKey())) {
+                    i = true;
+                } else {
+                    return false;
+                }
+            }else if (a == 'p') {
+                if (str.equals(ele.getValue())) {
+                    i = true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return i;
     }
 }
