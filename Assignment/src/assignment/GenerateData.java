e0996381a7001c9dee8a3631d160a4a7c23e4981
Phuc Lee <lephuc2605@gmail.com>
2020-11-10 16:55:21 +0700

    update

---
+++ Assignment/src/assignment/GenerateData.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class GenerateData {
+    public static void main(String[] args) {
+        String[] ho = {"Le", "Nguyen", "Ho", "Dinh", "Bui", "Do", "Phan", "Pham", "Hoang", "Trinh", };
+        String[] dem = {"Hoang", "Anh", "Quoc","Manh","Tien","Thu","Minh","Thi","Van","Thanh"};
+        String[] ten = {"Nhi","Anh","Phuc","Trang","Lan","Tien","Thu","Minh","Lam","Tra","Hai","Ha","Hanh","Tam","Thuy","Phong","Vu","Hieu","Trung"};
+        String[] roll = {"HE", "SE", "MK", "EN"};
+        
+    }
+}
